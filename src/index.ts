551069a14f0dfe66da3da9806c1e274250f507c5
BenLee3 <bendesigns76@gmail.com>
2019-06-02 10:56:28 +0900

    Refactor algorithm

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,12 @@ const zones: number[] = [0, 0.55, 0.75, 0.90, 1.05, 1.2];
 export function calculateZones(ftp: number) {
   if (ftp <= 0) return null;
   const results: number[] = [];
-  const lows = zones.map((zone) => ftp * zone);
+  const newFtp = Math.round(ftp)
+  const lows: number[] = zones.map((zone) => newFtp * zone);
   lows.map((low) => {
   	results.push(Math.floor(low - 1), Math.floor(low))
   });
-  results.shift();
-  results.pop();
-  return results;
+  const finalResults: number[] = results.slice(1,11)
+  return finalResults;
 }
 
--- src/tests/index.ts
+++ src/tests/index.ts
@@ -2,9 +2,9 @@ import { calculateZones } from '../';
 import { testCase1 } from './fixtures/test-cases';
 
 test('test with FTP number 234', () => {
-  const results = calculateZones(234);
-  for (let i = 0; i < results.length; i++) {
-    expect(results[i]).toBe(testCase1[i]);
+  const finalResults = calculateZones(234);
+  for (let i = 0; i < finalResults.length; i++) {
+    expect(finalResults[i]).toBe(testCase1[i]);
   }
 });
 
@@ -12,6 +12,19 @@ test('test with negative numbers', () => {
   const results = calculateZones(-234);
   expect(results).toBe(null);
 });
+
+test('test with 0', () => {
+  const results = calculateZones(0);
+  expect(results).toBe(null);
+});
+
+test('test with a number plus decimal', () => {
+  const results = calculateZones(234.4);
+  for (let i = 0; i < results.length; i++)
+  expect(results[i]).toBe(testCase1[i])
+})
+
+
 
 
 
